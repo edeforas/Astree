0e35f7455faefbf4d02986fbc931d9f89d8feae2
Etienne de Foras <etienne.deforas@gmail.com>
2021-01-29 19:51:46 +0100

    more python tests

--- python/Astree.py
+++ python/Astree.py
@@ -2,21 +2,24 @@ import numpy as np
 
 INFINITE=9.e999
 NB_LIGHT=3 # from center to edge
+YELLOW=560.e-9# 560 nanometers
 
 #################################################################################################
 
 class Device:
-    surfaces=list()
-    fov=0
+    surfaces=[]
+    fov=0.
     must_recompute=True
     iq=None
 
-    def add_surface(self,type,ticks=0,rcurv=INFINITE,diameter=INFINITE,conic=0):
+    def add_surface(self,surf_type,ticks=0,rcurv=INFINITE,diameter=INFINITE,conic=0.):
+        s=Surface(surf_type,rcurv,diameter)
+        self.surfaces.append(s)
         self.must_recompute=True
 
     def set_field_of_view(self,fov):
-        self.must_recompute=True
         self.fov=fov
+        self.must_recompute=True
 
     def image_quality(self):
         if self.must_recompute:
@@ -25,24 +28,50 @@ class Device:
         return self.iq
 
     def compute_quality(self):
-        self.iq=ImageQuality()
+        if len(self.surfaces)==0:
+            self.lf=-1
+            return
+
+        l=Light(self.surfaces[0].diameter)
+
+        for s in self.surfaces:
+            s.receive(l)
+
+        self.lf=l.lf()
+
+    def lf(self):
+        self.compute_quality()
+        return self.lf
+
 
 #################################################################################################
-
 class Light:
-    photons_pos=None
-    photons_dir=None
-    photons_lambda=None
-    photons_valid=None
+    def __init__(self,diameter, nb_photons=31): # no tilt for now
+        nb_total=nb_photons*nb_photons
+        self.dx=np.zeros(nb_total)
+        self.dy=np.zeros(nb_total)
+        self.dz=np.ones(nb_total)
+        
+        xd=np.linspace(-diameter,diameter,nb_photons)
+        yd=np.linspace(-diameter,diameter,nb_photons)
+        self.x,self.y=np.meshgrid(xd,yd)
+        self.z=np.zeros(nb_total)
+        self.valid=np.ones(nb_total)
+        self.wavelenght=np.ones(nb_total)*YELLOW
+        self.index=np.ones(nb_total)
 
-    def init(self,diameter,tilt):
-        pass
-
+    def lf(self):
+        return -1.
 #################################################################################################
 
 class Surface:
     type="stop"
     
+    def __init__(self,surf_type,rcurv,diameter):
+        self.type=surf_type
+        self.rcurv=rcurv
+        self.diameter=diameter
+
     def receive(self,light):
         if self.type=="stop" :
             self.stop(light)
--- python/test_newton.py
+++ python/test_newton.py
@@ -1,10 +1,9 @@
 import Astree as astree
 
-n=astree.Device()
-n.set_field_of_view(1.)
+d=astree.Device()
+d.set_field_of_view(1.)
 
-n.add_surface('reflect',1200,2400,200)
-n.add_surface('image')
+d.add_surface('reflect',1200,2400,200)
+d.add_surface('image')
 
-iq=n.image_quality()
-print(iq.maxlfro)
+print(d.lf())
